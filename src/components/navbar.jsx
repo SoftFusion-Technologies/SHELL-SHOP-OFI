c3dce338fb4ab2764eb37527a81bfb1f60b49a1c
Alvaro Aguilar <alvaroa305@gmail.com>
2024-07-25 23:17:31 -0300

    Actualizado el Navbar con un fondo difuminado y colores más legibles.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,11 +4,10 @@ import menu from '../assets/menu.png';
 
 const Navbar = () => {
   return (
-    <nav className="absolute top-0 w-full bg-black-400 border-b-2 border-gray-200 px-2 sm:px-4 lg:px-20 py-3">
+    <nav className="fixed top-0 w-full bg-opacity-70 backdrop-blur-md bg-black-400 border-b-2 border-gray-200 px-2 sm:px-4 lg:px-20 py-3">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <a href="/" className="flex items-center">
           <img src={logo} className="h-6 sm:h-9" alt="Logo" />
-          {/* <span className="pl-5 self-center text-xl font-semibold whitespace-nowrap">Shell Shop</span> */}
         </a>
         <div className="flex md:hidden">
           <button 
@@ -24,17 +23,17 @@ const Navbar = () => {
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="flex flex-col mt-4 pr-5 md:flex-row md:gap-20 md:mt-0 md:text-sm md:font-medium lg:text-base">
             <li>
-              <a href="/" className="block py-2 pr-4 pl-3 text-white rounded md:bg-transparent md:p-0 transition hover:text-red">
+              <a href="/" className="block py-2 pr-4 pl-3 text-red rounded md:bg-transparent md:p-0 transition hover:text-red">
               Home
               </a>
             </li>
             <li>
-              <a href="/nosotros" className="block py-2 pr-4 pl-3 text-white rounded md:bg-transparent md:p-0 transition hover:text-red">
+              <a href="/nosotros" className="block py-2 pr-4 pl-3 text-red rounded md:bg-transparent md:p-0 transition hover:text-red">
               Nosotros
               </a>
             </li>
             <li>
-              <a href="/contacto" className="block py-2 pr-4 pl-3 text-white rounded md:bg-transparent md:p-0 transition hover:text-red">
+              <a href="/contacto" className="block py-2 pr-4 pl-3 text-red rounded md:bg-transparent md:p-0 transition hover:text-red">
               Contacto
               </a>
             </li>
